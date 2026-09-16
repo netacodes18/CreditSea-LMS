f8a3bce32da585ccb6293232be439aaac1afcd23
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-09-16 15:11:32 +0530

    Fix Cloudinary PDF upload resource_type

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -13,7 +13,7 @@ cloudinary.config({
 export const uploadToCloudinary = (buffer: Buffer, folder: string): Promise<any> => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { folder },
+      { folder, resource_type: 'auto' },
       (error, result) => {
         if (result) resolve(result);
         else reject(error);
